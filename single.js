fe0ad822bb9e11edcffd555870e2eaee06ac6f0b
Magnus Madsen <magn5310@stud.kea.dk>
2023-02-22 15:54:56 +0100

    js stuff

--- single.js
+++ single.js
@@ -1,19 +1,22 @@
 const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
-const url = `https://tema7-7d73.restdb.io/rest/hidden-gems${_id}`;
+const id = urlParams.get("_id");
+const url = `https://tema7-7d73.restdb.io/rest/hidden-gems/${_id}`;
+
 const options = {
   headers: {
     "x-apikey": "63f293b7478852088da6845f",
   },
 };
-async function hentFerie() {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  visFerie(data);
+
+async function hentData() {
+  const respons = await fetch(url, options);
+  const json = await respons.json();
+  json.forEach(visFerie);
 }
 
+hentData();
+
 function visFerie(ferie) {
-  document.querySelector(".grid-img img").src = ferie.billede;
+  document.querySelector(".grid-img img").src = "billeder/" + ferie.billede;
+  document.querySelector(".grid-text-view p").textContent = ferie.bio;
 }
-hentFerie();
