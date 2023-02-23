4771ec7bd7c60ea33708adb83c34c813b29bb050
magnus madsen <magn@stud.kea.dk>
2023-02-23 13:35:26 +0100

    lets go

--- single.js
+++ single.js
@@ -21,10 +21,12 @@ function visFerie(json) {
       document.querySelector(".grid-img-view img").src = "billeder/" + ferie.billede;
       document.querySelector(".grid-text-view p").textContent = ferie.bio;
       document.querySelector(".heroh1").textContent = ferie.by;
+      document.querySelector(".h2view").textContent = ferie.by;
       document.querySelector(".welcome").textContent = ferie.land;
       document.querySelector(".aktiv").textContent = ferie.aktiv;
       document.querySelector(".mad").textContent = ferie.mad;
       document.querySelector(".hotel").textContent = ferie.hotel;
+      document.querySelector(".h1view").textContent = ferie.land;
       document.querySelector(".aktivimg").src = "billeder/" + ferie.aktivimg;
       document.querySelector(".hotelimg").src = "billeder/" + ferie.hotelimg;
       document.querySelector(".madimg").src = "billeder/" + ferie.madimg;
